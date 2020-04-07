ca1793140811caf531dd2497b9aa0bca1e3d71a5
Sahirraj <mahmoodsamar786@gmail.com>
2020-04-08 02:13:42 +0530

    shared

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,9 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static com.example.myapplication.prefConstant.IS_LOGGED_IN;
+
 public class LoginActivity extends AppCompatActivity {
     EditText editTextFullName, editTextUserName;
     Button buttonLogin;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,8 @@ public class LoginActivity extends AppCompatActivity {
         editTextFullName = findViewById(R.id.editTextFullName);
         editTextUserName = findViewById(R.id.editTextUserName);
         buttonLogin = findViewById(R.id.buttonLogin);
+
+        setupSharedPreferences();
 
         View.OnClickListener clickAction = new View.OnClickListener() {
             @Override
@@ -29,9 +36,12 @@ public class LoginActivity extends AppCompatActivity {
                 String userName = editTextUserName.getText().toString();
 
                 if(!TextUtils.isEmpty(fullName) && !TextUtils.isEmpty(userName)){
-                    Intent intent = new Intent(LoginActivity.this,MyNotesActivity.class);
-                    intent.putExtra("full_name",fullName);
+                    Intent intent = new Intent(LoginActivity.this, MyNotesActivity.class);
+                    intent.putExtra(AppConstant.FULL_NAME,fullName);
                     startActivity(intent);
+                    saveLoginStatus();
+                    saveFullName(fullName);
+
                 }
                 else{
                     Toast.makeText(LoginActivity.this,"Please Enter Valid Fullname And Username",Toast.LENGTH_SHORT).show();
@@ -40,5 +50,22 @@ public class LoginActivity extends AppCompatActivity {
         };
         buttonLogin.setOnClickListener(clickAction);
 
+    }
+
+    private void saveFullName(String fullName) {
+        editor = sharedPreferences.edit();
+        editor.putString(prefConstant.FULL_NAME,fullName);
+        editor.apply();
+
+    }
+
+    private void saveLoginStatus() {
+        editor = sharedPreferences.edit();
+        editor.putBoolean(IS_LOGGED_IN,true);
+        editor.apply();
+    }
+
+    private void setupSharedPreferences() {
+        sharedPreferences = getSharedPreferences(prefConstant.SHARED_PREFERENCE_NAME,MODE_PRIVATE);
     }
 }
--- app/src/main/java/com/example/myapplication/MyNotesActivity.java
+++ app/src/main/java/com/example/myapplication/MyNotesActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,52 +18,71 @@ import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.w3c.dom.Text;
+
 public class MyNotesActivity extends AppCompatActivity {
+
     String fullName;
     FloatingActionButton fabAddNotes;
-    TextView textViewTitle,textViewDescription;
+    TextView textViewTitle, textViewDescription;
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_notes);
-        fabAddNotes = findViewById(R.id.fabAddNotes);
-        textViewTitle = findViewById(R.id.textViewTitle);
-        textViewDescription = findViewById(R.id.textviewDescription);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_my_notes );
 
-       Intent intent = getIntent();
+        bindView();
+        getIntentData();
+        setupSharedPreference();
 
-        fullName =  intent.getStringExtra("full_name");
-
-        fabAddNotes.setOnClickListener(new View.OnClickListener() {
+        fabAddNotes.setOnClickListener( new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 setupDialogBox();
             }
-        });
+        } );
 
-        getSupportActionBar().setTitle(fullName);
+        getSupportActionBar().setTitle( fullName );
+    }
+
+    private void setupSharedPreference() {
+        sharedPreferences = getSharedPreferences(prefConstant.SHARED_PREFERENCE_NAME,MODE_PRIVATE);
+    }
+
+    private void getIntentData() {
+        Intent intent = getIntent();
+        fullName = intent.getStringExtra( AppConstant.FULL_NAME );
+        if(TextUtils.isEmpty(fullName)){
+            fullName = sharedPreferences.getString(prefConstant.FULL_NAME,"");
+        }
+    }
+
+    private void bindView() {
+        fabAddNotes = findViewById( R.id.fabAddNotes );
+        textViewTitle = findViewById( R.id.textViewTitle );
+        textViewDescription = findViewById( R.id.textViewDescription );
+
     }
 
     private void setupDialogBox() {
-        View view = LayoutInflater.from(MyNotesActivity.this).inflate(R.layout.add_notes_dialog_layout,null);
-        final EditText editTextTitle = view.findViewById(R.id.editTextTitle);
-        final EditText editTextDescription = findViewById(R.id.editTextDescription);
-        Button buttonSubmit = view.findViewById(R.id.buttonSubmit);
-        //dialog creation
-        AlertDialog dialog = new AlertDialog.Builder(this)
-                .setView(view)
-                .setCancelable(false)
-                .create();
-        buttonSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String title = editTextTitle.getText().toString();
-                String desc = editTextDescription.getText().toString();
-              //  Log.d("MyNotesActivity",title);
-               // Log.d("MyNotesActivity",desc);
-            }
-        });
-        dialog.show();
+        View view = LayoutInflater.from( MyNotesActivity.this ).inflate( R.layout.add_notes_dialog_layout,null );
+        final EditText editTextTitle = view.findViewById( R.id.editTextTitle );
+        final EditText editTextDescription = view.findViewById( R.id.editTextDescription );
+        Button buttonSubmit = view.findViewById( R.id.buttonSubmit );
 
+        final AlertDialog dialog = new AlertDialog.Builder( this )
+                .setView( view )
+                .setCancelable( false )
+                .create();
+        buttonSubmit.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewTitle.setText( editTextTitle.getText().toString() );
+                textViewDescription.setText( editTextDescription.getText().toString() );
+                dialog.hide();
+            }
+        } );
+        dialog.show();
     }
 }
